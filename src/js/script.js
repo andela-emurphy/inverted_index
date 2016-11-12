0024a5fd756bde78fff9c18deaddf2f56b8aa855
enaho murphy <enahomurphy@gmail.com>
2016-11-12 07:38:05 +0100

    upload a json file

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,15 @@
 var userDocments = document.getElementById('file')
 
 userDocments.addEventListener('change', function(evt) {
+
   console.log(this.files)
-
-
+  
+ for(let file in this.files) {
+    var reader = new FileReader()
+    console.log(this.files[file])
+    reader.onload = function(event) {
+      console.log(event.target.result)
+    }
+    reader.readAsText(this.files[file])
+ }
 });
